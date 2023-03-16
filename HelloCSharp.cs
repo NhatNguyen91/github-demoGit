18b8ad0d187977368d1791c69fd81d6e2dce57f6
Nhat Nguyen <nhatnguyen3150@gmail.com>
2023-03-16 17:07:20 +0700

    committing   files

--- HelloCSharp.cs
+++ HelloCSharp.cs
@@ -14,9 +14,19 @@ public class Class1
 
 	}
 
-	public void addMoreSource(()=>{ Console.WriteLine("Add more source!")});
+	public void addMoreSource(Action callback , int a)
+	{
+		callback() ;
+	}
 	void Update()
 	{
+		int a=0 ;
 		Console.WriteLine("Update availbe!");
+		addMoreSource(() => Console.WriteLine("Call Back"), a);
+	}
+
+	void main()
+	{
+		addDick();
 	}
 }

463d1d04c456dba6ce2d3ad43fb4e25e2c0468ec
NhatNguyen91 <39874142+NhatNguyen91@users.noreply.github.com>
2023-03-23 09:45:45 +0700

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -10,7 +10,7 @@ public class NewClass
 
 	public Action action = () => Console.WriteLine("Update All Files and More");
 =======
-	public Action action = ()=> Console.WriteLine("Update All Files and more");
+	public Action action = ()=> Console.WriteLine("Update All Files and more and moreeeeeee");
 >>>>>>> 3f4d9ebdd6eb96bdca7ff45281621934a84234f1
 }
 	
